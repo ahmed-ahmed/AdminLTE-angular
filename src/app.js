2bd1fac557b13d505cc885330a120940a1d6eb3d
Ahmed M Ahmed <aahmed@cas.org>
2016-03-31 16:33:26 -0400

    initial code based on empty angular template

---
+++ src/app.js
@@ -0,0 +1,12 @@
+angular.module('app', ['ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'home/home.html',
+        controller: 'HomeController',
+        controllerAs: 'home'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
